686d861b76d95b5176adc6010d0a039ece9b4d61
David Zhu <junjiezhu1388@gmail.com>
2024-03-19 22:04:41 -0700

    added homebar

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,32 +1,37 @@
 import Table from 'react-bootstrap/Table';
 
-const sizeHeight = 15;
+import Navbar from '../components/Homebar.jsx';
+
+const sizeHeight = 52;
 const sizeWidth = 7;
 
 const Days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 function ResponsiveExample() {
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          {Array.from({ length: sizeWidth }).map((_, index) => (
-            <th key={index}>{Days[index]}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {Array.from({ length: sizeHeight }).map((_, indexHeight) => (
+    <div>
+      <Navbar/>
+      <Table striped bordered borderColor="white" hover size='xxl' style={{justifySelf: 'center'}}>
+        <thead>
           <tr>
-            <td key={indexHeight}>{indexHeight}</td>
-            {Array.from({ length: sizeWidth }).map((_, indexWidth) => (
-              <td key={indexWidth}>Table cell {indexWidth}</td>
+            <th>Week</th>
+            {Array.from({ length: sizeWidth }).map((_, index) => (
+              <th key={index}>{Days[index]}</th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {Array.from({ length: sizeHeight }).map((_, indexHeight) => (
+            <tr>
+              <td key={indexHeight}>{indexHeight+1}</td>
+              {Array.from({ length: sizeWidth }).map((_, indexWidth) => (
+                <td key={indexWidth}>Table cell {indexWidth+1}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
   );
 }
 
