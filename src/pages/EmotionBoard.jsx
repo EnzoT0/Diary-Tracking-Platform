43aba69772fe28133aa0f0d25627040b7328f047
David Zhu <junjiezhu1388@gmail.com>
2024-03-21 11:51:41 -0700

    can now navigate to pages

--- src/pages/EmotionBoard.jsx
+++ src/pages/EmotionBoard.jsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react';
+import { useSearchParams } from "react-router-dom";
 import Table from 'react-bootstrap/Table';
 
 const Columnsnames =  ["EmotionID", "Subtypes", "OverallType"];
