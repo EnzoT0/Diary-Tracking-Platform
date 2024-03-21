84b14234cc26b01ed73adcc3388ccdbf8c619946
David Zhu <junjiezhu1388@gmail.com>
2024-03-21 15:58:02 -0700

    issueBoard

---
+++ src/pages/IssueBoard.jsx
@@ -0,0 +1,47 @@
+import Table from 'react-bootstrap/Table';
+import Homebar from '../components/Homebar';
+
+const Columnnames =  ["IssueID", "Name", "Resolved", "Date", "Details"];
+
+function DisplayIssues(year) {
+
+    const mockup = [{
+        IssueID: 1,
+        Name: "mock1",
+        Resolved: true,
+        Date: Date('January 20, 2020'),
+        Details: "HelloWorld"
+    }, {
+        IssueID: 2,
+        Name: "mock2",
+        Resolved: false,
+        Date: Date('January 21, 2020'),
+        Details: "HelloWorld1"
+    }];
+
+    return (
+        <>
+        <Homebar />
+        <Table striped bordered borderColor="white" hover size='xxl' style={{justifySelf: 'center'}}>
+        <thead>
+          <tr>
+            {Columnnames.map((colname) => (
+              <th key = {colname}> {colname} </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {mockup.map((queryresult) => (
+            <tr>
+              {Columnnames.map((colname) => (
+              <th key = {colname}> {queryresult[colname]} </th>
+            ))}
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      </>
+    )
+}
+
+export default DisplayIssues
