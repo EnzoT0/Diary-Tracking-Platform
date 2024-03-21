1ad831141ff7f36c54d9855d320d39cf5b18c049
David Zhu <junjiezhu1388@gmail.com>
2024-03-20 23:07:18 -0700

    stuffs added to m3

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import Menu from './pages/Menu.jsx';
 import Calendar from './pages/Calendar.jsx'
 import Diary from './pages/Diary.jsx';
 import Goallist from './pages/Goallist.jsx';
+import CalendarNew from './pages/CalendarNew.jsx';
 
 const App = () => (
     <Router>
         <Routes>
             <Route exact path="/" element={<Menu />} />
-            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/calendar" element={<CalendarNew />} />
             <Route path="/diary" element={<Diary />} />
             <Route path="/goallist" element={<Goallist />} />
         </Routes>
--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -2,30 +2,86 @@ import Table from 'react-bootstrap/Table';
 
 import Navbar from '../components/Homebar.jsx';
 
-const sizeHeight = 52;
-const sizeWidth = 7;
+const sizeHeight = 52
+const sizeWidth = 6
 
-const Days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const Types = ['Year', 'YearTheme', 'Date', 'Summary', 'EmotionBoard', 'IssueBoard'];
 
 function ResponsiveExample() {
+  const fetchData = [{
+    "Year": "2021",
+    "YearTheme": "New Beginnings",
+    "Date": "01/01/2021",
+    "Summary": "First day of the year",
+    "EmotionBoard": "Happy",
+    "IssueBoard": "None"
+  },
+  {
+    "Year": "2021",
+    "YearTheme": "New Beginnings",
+    "Date": "01/02/2021",
+    "Summary": "Second day of the year",
+    "EmotionBoard": "Happy",
+    "IssueBoard": "None"
+  },
+  {
+    "Year": "2021",
+    "YearTheme": "New Beginnings",
+    "Date": "01/03/2021",
+    "Summary": "Third day of the year",
+    "EmotionBoard": "Happy",
+    "IssueBoard": "None"
+  },
+  {
+    "Year": "2021",
+    "YearTheme": "New Beginnings",
+    "Date": "01/04/2021",
+    "Summary": "Fourth day of the year",
+    "EmotionBoard": "Happy",
+    "IssueBoard": "None"
+  },
+  {
+    "Year": "2021",
+    "YearTheme": "New Beginnings",
+    "Date": "01/05/2021",
+    "Summary": "Fifth day of the year",
+    "EmotionBoard": "Happy",
+    "IssueBoard": "None"
+  },
+  {
+    "Year": "2021",
+    "YearTheme": "New Beginnings",
+    "Date": "01/06/2021",
+    "Summary": "Sixth day of the year",
+    "EmotionBoard": "Happy",
+    "IssueBoard": "None"
+  },
+  {
+    "Year": "2021",
+    "YearTheme": "New Beginnings",
+    "Date": "01/07/2021",
+    "Summary": "Seventh day of the year",
+    "EmotionBoard": "Happy",
+    "IssueBoard": "None"
+  }]
+
+
   return (
     <div>
       <Navbar/>
       <Table striped bordered borderColor="white" hover size='xxl' style={{justifySelf: 'center'}}>
         <thead>
           <tr>
-            <th>Week</th>
             {Array.from({ length: sizeWidth }).map((_, index) => (
-              <th key={index}>{Days[index]}</th>
+              <th key={index}>{Types[index]}</th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {Array.from({ length: sizeHeight }).map((_, indexHeight) => (
-            <tr>
-              <td key={indexHeight}>{indexHeight+1}</td>
-              {Array.from({ length: sizeWidth }).map((_, indexWidth) => (
-                <td key={indexWidth}>Table cell {indexWidth+1}</td>
+          {fetchData.map((data, index) => (
+            <tr key={index}>
+              {Object.values(data).map((value, index) => (
+                <td key={index}>{value}</td>
               ))}
             </tr>
           ))}
